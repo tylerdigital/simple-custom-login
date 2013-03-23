0c1eba7965cecb7ce42192e550bbd052dde88680
Nathan Tyler <croixhaug@gmail.com>
2013-03-23 10:53:33 -0700

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,189 @@
+<?php
+/*
+Plugin Name: TD Custom Login
+Plugin URI: http://tylerdigital.com
+Description: Easy login screen customization
+Version: 1.0
+Author: Tyler Digital
+Author URI: http://tylerdigital.com
+License:
+
+  Copyright 2013 Tyler Digital
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+*/
+
+class TDCustomLogin {
+	 
+	/*--------------------------------------------*
+	 * Constructor
+	 *--------------------------------------------*/
+	
+	/**
+	 * Initializes the plugin by setting localization, filters, and administration functions.
+	 */
+	function __construct() {
+		
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'plugin_textdomain' ) );
+		add_filter( 'login_body_class', array( $this, 'apply_color_style' ) );
+
+		// Register admin styles and scripts
+		add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
+
+		add_action( 'login_head', array( $this, 'register_login_styles' ) );
+	
+		// Register site styles and scripts
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
+	
+		// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+		register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
+		
+	    /*
+	     * TODO:
+	     * Define the custom functionality for your plugin. The first parameter of the
+	     * add_action/add_filter calls are the hooks into which your code should fire.
+	     *
+	     * The second parameter is the function name located within this class. See the stubs
+	     * later in the file.
+	     *
+	     * For more information: 
+	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
+	     */
+	    add_action( 'TODO', array( $this, 'action_method_name' ) );
+	    add_filter( 'TODO', array( $this, 'filter_method_name' ) );
+
+	} // end constructor
+	
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 */
+	public function activate( $network_wide ) {
+		// TODO:	Define activation functionality here
+	} // end activate
+	
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 */
+	public function deactivate( $network_wide ) {
+		// TODO:	Define deactivation functionality here		
+	} // end deactivate
+	
+	/**
+	 * Fired when the plugin is uninstalled.
+	 *
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 */
+	public function uninstall( $network_wide ) {
+		// TODO:	Define uninstall functionality here		
+	} // end uninstall
+
+	/**
+	 * Loads the plugin text domain for translation
+	 */
+	public function plugin_textdomain() {
+	
+		// TODO: replace "td-custom-login-locale" with a unique value for your plugin
+		$domain = 'td-custom-login-locale';
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+        load_textdomain( $domain, WP_LANG_DIR.'/'.$domain.'/'.$domain.'-'.$locale.'.mo' );
+        load_plugin_textdomain( $domain, FALSE, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+
+	} // end plugin_textdomain
+
+	/**
+	 * Registers and enqueues admin-specific styles.
+	 */
+	public function register_login_styles() {
+	
+		wp_enqueue_style( 'td-custom-login-login-styles', plugins_url( 'td-custom-login/css/login.css' ) );
+	
+	} // end register_admin_styles
+
+	public function register_admin_styles() {
+	
+		wp_enqueue_style( 'td-custom-login-admin-styles', plugins_url( 'td-custom-login/css/admin.css' ) );
+	
+	} // end register_admin_styles
+
+	/**
+	 * Registers and enqueues admin-specific JavaScript.
+	 */	
+	public function register_admin_scripts() {
+	
+		wp_enqueue_script( 'td-custom-login-admin-script', plugins_url( 'td-custom-login/js/admin.js' ) );
+	
+	} // end register_admin_scripts
+	
+	/**
+	 * Registers and enqueues plugin-specific styles.
+	 */
+	public function register_plugin_styles() {
+	
+		// TODO:	Change 'td-custom-login' to the name of your plugin
+		wp_enqueue_style( 'td-custom-login-plugin-styles', plugins_url( 'td-custom-login/css/display.css' ) );
+	
+	} // end register_plugin_styles
+	
+	/**
+	 * Registers and enqueues plugin-specific scripts.
+	 */
+	public function register_plugin_scripts() {
+	
+		// TODO:	Change 'td-custom-login' to the name of your plugin
+		wp_enqueue_script( 'td-custom-login-plugin-script', plugins_url( 'td-custom-login/js/display.js' ) );
+	
+	} // end register_plugin_scripts
+	
+	/*--------------------------------------------*
+	 * Core Functions
+	 *---------------------------------------------*/
+	
+	/**
+ 	 * NOTE:  Actions are points in the execution of a page or process
+	 *        lifecycle that WordPress fires.
+	 *
+	 *		  WordPress Actions: http://codex.wordpress.org/Plugin_API#Actions
+	 *		  Action Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
+	 *
+	 */
+	function apply_color_style( $classes ) {
+		$classes[] = 'red';
+		return $classes;
+	} // end action_method_name
+	
+	/**
+	 * NOTE:  Filters are points of execution in which WordPress modifies data
+	 *        before saving it or sending it to the browser.
+	 *
+	 *		  WordPress Filters: http://codex.wordpress.org/Plugin_API#Filters
+	 *		  Filter Reference:  http://codex.wordpress.org/Plugin_API/Filter_Reference
+	 *
+	 */
+	function filter_method_name() {
+	    // TODO:	Define your filter method here
+	} // end filter_method_name
+  
+} // end class
+
+$plugin_name = new TDCustomLogin();
