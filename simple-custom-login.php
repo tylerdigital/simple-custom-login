0bd7be826616f0fce7dcd3743832278ab551567d
Nathan Tyler <croixhaug@gmail.com>
2013-09-30 16:18:05 -0700

    Rename file to new slug

--- simple-custom-login.php
+++ simple-custom-login.php
@@ -58,6 +58,8 @@ class TDCustomLogin {
 		register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
 
 		include_once plugin_dir_path( __FILE__ ) . 'includes/settings.php';
+		if ( !class_exists( 'CWS_Login_Logo_Plugin' ) ) include_once( 'lib/login-logo/login-logo.php' );
+
 		$this->settings = new TDCustomLogin_Settings( $this );
 
 		/*
