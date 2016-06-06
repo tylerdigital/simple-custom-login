96a02c3ad251b61bb0a42c05f5ca2efa44013c97
Nathan Tyler <croixhaug@gmail.com>
2016-06-05 17:08:28 -0700

    Fix internationalization output

--- lib/options-buddy-master/fields/image.php
+++ lib/options-buddy-master/fields/image.php
@@ -36,7 +36,7 @@ class OptionsBuddy_Settings_Field_Image extends OptionsBuddy_Settings_Field{
        </div>
     <?php
         echo  "<input type='hidden' class='hidden-image-url' id='{$id}' name='{$id}' value='{$value}'/>";
-        echo  '<input type="button" class="button settings-upload-image-button" id="'. $id .'_button" value="'. _e('Browse', 'simple-custom-login').'" data-id="'.$id.'" data-btn-title="'._e('Select', 'simple-custom-login').'" data-uploader-title="'._e('Select', 'simple-custom-login').'" />';
+        echo  '<input type="button" class="button settings-upload-image-button" id="'. $id .'_button" value="'. __('Browse', 'simple-custom-login').'" data-id="'.$id.'" data-btn-title="'.__('Select', 'simple-custom-login').'" data-uploader-title="'.__('Select', 'simple-custom-login').'" />';
        
        
      
